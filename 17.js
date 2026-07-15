dc8538803e3c5aeedf202fc43018f0f104e02510
tsherabs <jdamchoe@gmail.com>
2026-07-15 19:12:00 +0600

    Updated 17

--- 17.js
+++ 17.js
@@ -1,2 +1,17 @@
-let x = 0; 
-console.log(typeof x)
+const prompt = require('prompt-sync')(); 
+
+let userCharacter = 'a'; 
+let dummyChracter = 1; 
+let dummyChracter1 = 'a'; 
+checkCharactertype(userCharacter, dummyChracter, dummyChracter1); 
+
+
+function checkCharactertype(character, dummyChracter, dummyChracter1){
+    if(typeof character === typeof dummyChracter){
+        console.log("The enterted character is a number");
+    }else if(typeof character === typeof dummyChracter1){
+        console.log("The enterted character is a string");
+    }else{
+        console.log("The enterted character is a special character");
+    }
+}
